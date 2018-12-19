89e082986ccc2582a3ac204734f2b2937e36d877
Ferran <ferranlao@hotmail.com>
2018-12-19 18:52:37 +0100

    allmeals maquetado

--- proyecto/public/javascripts/allplatos.js
+++ proyecto/public/javascripts/allplatos.js
@@ -1,9 +1,10 @@
-document.querySelectorAll(".recipeRemove").forEach(e=>{
-    e.addEventListener("click",(e)=>{
-      const name =e.target.parentNode.querySelector("p").innerText
+document.querySelectorAll(".recipeRemove").forEach(e => {
+  e.addEventListener("click", e => {
+      const name =e.target.parentNode.parentNode.querySelector("p").innerText
         axios.post("/logged/recipeRemove",{name}).then(()=>{
-         
         })
-        e.target.parentNode.parentNode.removeChild( e.target.parentNode)
-    })
-})
+    e.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+      e.target.parentNode.parentNode.parentNode.parentNode
+    );
+  });
+});
