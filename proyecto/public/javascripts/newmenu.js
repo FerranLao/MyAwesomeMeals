7ba4409d1aff041624612780a3af7295c20e5453
Ferran <ferranlao@hotmail.com>
2018-12-19 20:53:50 +0100

    maquentando a tope

--- proyecto/public/javascripts/newmenu.js
+++ proyecto/public/javascripts/newmenu.js
@@ -1,17 +1,43 @@
 let menu = [];
 document.querySelectorAll(".addtochart").forEach(e => {
   e.addEventListener("click", e => {
-    
-    const name = e.target.parentNode.querySelector("p").innerText;
+    const name = e.target.parentNode.parentNode.querySelector("p").innerText;
     menu.push(name);
-    const list = document.querySelector(".menulist");
-    list.innerHTML += `<p>${name}</p>`;
+    printmenu()
+    // const list = document.querySelector(".menulist");
+    // list.innerHTML += `<p>${name}</p>`;
   });
 });
 
 document.querySelector(".finishchart").addEventListener("click", e => {
-  const name=document.querySelector(".menuname").value
-  axios.post("/logged/newmenu", { menu,name }).then(menu => {
+  const name = document.querySelector(".menuname").value;
+  axios.post("/logged/newmenu", { menu, name }).then(menu => {
     window.location.replace(`/logged/charts/${menu.data}`);
   });
 });
+
+
+const printmenu = () => {
+  let menuHTML = document.querySelector(".menulist");
+  menuHTML.innerHTML = "";
+  menuHTML.innerHTML += `<ul class="collection with-header">
+  <li class="collection-header"><h4>Your menu</h4></li>`;
+  menu.forEach(e => {
+    if (e != undefined) {
+      menuHTML.innerHTML += `<li class="collection-item"><div>${e}<a href="#!" class="secondary-content listdel"><i class="material-icons ">X</i></a></div></li>`;
+    }
+  });
+  menuHTML.innerHTML += `</ul>`;
+  arrdelete();
+};
+const arrdelete = () => {
+  document.querySelectorAll(".listdel").forEach(e => {
+    e.addEventListener("click", e => {
+      let text = e.target.parentNode.parentNode.innerText;
+      text = text.split("\nX")
+      const indexof=menu.indexOf(text[0])
+      menu.splice(indexof, 1);
+      printmenu()
+    });
+  });
+};
