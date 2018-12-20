411060694b6d3dd3d0c1a38105275ae0c52908f7
AaronReina <aaron.ivan.reina@gmail.com>
2018-12-20 17:57:39 +0100

    problems

--- proyecto/public/javascripts/script.js
+++ proyecto/public/javascripts/script.js
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-// document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.sidenav');
-//   var instances = M.Sidenav.init(elems, {});
-// });
-
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-});
-
-=======
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
         
->>>>>>> Ferran
